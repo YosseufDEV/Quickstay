99244f8847806c0a787fd52db158b927a31519ba
YosseufDEV <nameyinx@gmail.com>
2026-06-11 12:03:22 +0300

    Finished Hotels

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,9 +6,14 @@ import useAuthStore from "./stores/authStore";
 
 const App = () => {
     useEffect(() => {
-        getCurrentUser().then((user) => {;
-            if(user.user) {
-                useAuthStore.getState().setUser(user);
+        getCurrentUser().then((res) => {
+            if(res.status == 400 || res.status == 401) {
+                console.log(res.message); 
+            }
+
+            const { payload } = res;
+            if(payload.user) {
+                useAuthStore.getState().setUser(payload.user);
             }
         }).catch((error) => {
             console.error("Failed to fetch current user:", error);
